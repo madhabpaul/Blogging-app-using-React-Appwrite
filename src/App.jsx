81e3d585cab696d53e9ae06a904ec50a01a791c8
madhabpaul <madhab12paul@gmail.com>
2023-10-01 20:45:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import conf from './conf/conf'
+function App() {
+
+console.table(conf.appwriteUrl, conf.appwritepProjectId, conf.appwriteDatabaseId, conf.appwriteCollectionId, conf.appwriteBucketId)
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
